044f7e8fad3373726ffaad7767441961a302c466
Gulrez Bawa <gulrezsbawa@gmail.com>
2022-08-20 07:19:28 -0400

    feat(kr): initial commit

--- public/worker.js
+++ public/worker.js
@@ -1,5 +1,5 @@
 var CACHE_NAME = "pwa-task-manager";
-var urlsToCache = ["/kaveri_resume/", "/kaveri_resume/completed"];
+var urlsToCache = ["/kaveri_resume/"];
 
 // Install a service worker
 self.addEventListener("install", (event) => {
