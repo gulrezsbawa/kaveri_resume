77394bacd93b5ae517bdd48d271350d15214e3f4
Gulrez Bawa <gulrezsbawa@gmail.com>
2022-08-21 13:43:36 +0530

    feat(gr): Gulrez Resume

--- src/Components/constant.js
+++ src/Components/constant.js
@@ -1,1 +1,1 @@
-export const THEME_COLOR = "#f29e3b";
+export const THEME_COLOR = "#52b8d2"; // "#f29e3b";
--- src/Data/ApplicantData.js
+++ src/Data/ApplicantData.js
@@ -124,5 +124,137 @@ const KaveriData = {
     ],
   },
 };
+const GulrezData = {
+  applicantName: "Gulrez",
+  designation: "Senior Software Engineer",
+  briefSummary: `Full stack web developer, passion & expertise in process workflow automation. Love to
+    Code, Live to Code. Fast Learner. 2 Instant Puraskaar Award for performance
+    improvement. Handling Major Projects of Company with big responsibilities.`,
+  mail: "gulrez0491@gmail.com",
+  mobile: "+1-317-540-5173",
+  website: "gulrezbawa.com",
+  linkedIn: "linkedin.com/in/gulrez-bawa",
+  certifications: {
+    title: "CERTIFICATIONS",
+    list: [
+      {
+        subTitle: "DevOps Certified",
+        year: "2018",
+        details: [],
+      },
+    ],
+  },
+  education: {
+    title: "EDUCATION",
+    list: [
+      {
+        subTitle: "Master of Computer Application (MCA)",
+        year: "2016",
+        details: [],
+      },
+      {
+        subTitle: "Bachelor of Computer Application (BCA)",
+        year: "2012",
+        details: [],
+      },
+    ],
+  },
+  awards: {
+    title: "HONORS & AWARDS",
+    list: [
+      {
+        subTitle: "Instant Puraskaar Award 2 Time by Assistant Vice President",
+        year: "2018 & 2019",
+        details: [
+          `Improved in performance for issue ticketing application by
+          writing stimulation programs for sending/receiving 16K emails
+          to the application`,
+          `Developing Lazy Loading to improve performance`,
+          `Caching module to improve performance`,
+          `Creation of dynamic SQL E-Mail Template which is reused in all
+          applications`,
+          `Maintenance &amp; support for billing tool, handling process of
+          organization’s monthly billing.`,
+          `Training Management, Compliance Tool, Rest API`,
+        ],
+      },
+    ],
+  },
+  expertise: {
+    title: "EXPERTISE",
+    list: [
+      "React JS",
+      "Typescript",
+      "Javascript",
+      "HTML",
+      "CSS",
+      "DHTMLX",
+      "Outlook Add-In",
+      "React Final Form",
+      "BaseWeb Design",
+    ],
+  },
+  skills: {
+    title: "SKILLS",
+    list: [
+      "Critical Thinking",
+      "Problem solving",
+      "Interpersonal",
+      "Requirement Gathering & Analysis",
+      "Planning / Prioritizing",
+      "Decision Making",
+      "Good communication",
+      "Leadership",
+      "Collaboration",
+    ],
+  },
+  tools: {
+    title: "TOOLS",
+    list: [
+      "JIRA",
+      "MS Word",
+      "Excel",
+      "Powerpoint",
+      "Visio",
+      "SQL",
+      "Postman",
+      "Visual Studio Code",
+      "Visual Studio Professional",
+      "Android Studio",
+      "Unity 3D",
+      "Figma",
+    ],
+  },
+  professionServices: {
+    title: "PROFESSIONAL EXPERIENCE",
+    list: [
+      {
+        name: "Jifflenow Technology",
+        year: "2021 - Present",
+        subTitle: "Senior Software Engineer",
+        details: [
+          `Work as front end developer`,
+          `Working on customer facing modules`,
+          `Expertise in Time zone handling using Luxon Library`,
+          `Creation of Scheduler using DHTMLX library`,
+        ],
+      },
+      {
+        name: "Siemens Technology and Services Private Limited (STSPL)",
+        year: "2016 - 2021",
+        subTitle: "Senior Software Engineer",
+        details: [
+          `Full stack web developer responsible for end-to-end web app
+          development.`,
+          `Prototyped new product features to improve productivity`,
+          `Handling organization’s Employee Database and processing as per
+          different tools`,
+          `Boosted user experience (UX/UI) scores over company-wide
+          previous best`,
+        ],
+      },
+    ],
+  },
+};
 
-export const applicantData = KaveriData;
+export const applicantData = GulrezData;
