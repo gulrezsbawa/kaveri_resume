a05b074754c291bc726bc9b98de7d63fbdcd5240
Gulrez Bawa <gulrezsbawa@gmail.com>
2022-08-21 13:49:24 +0530

    feat(kr): data corrected

--- src/Data/ApplicantData.js
+++ src/Data/ApplicantData.js
@@ -1,4 +1,4 @@
-const KaveriData = {
+export const applicantData = {
   applicantName: "Kaveri",
   designation: "Senior Business Associate",
   briefSummary:
@@ -91,12 +91,12 @@ const KaveriData = {
         subTitle: "Senior Associate Consultant",
         details: [
           `Owning and Operating the systems which ensures comprehensive
-           and high-quality data submissions to the states accordingly to
-           stringent data quality standards laid down by each state`,
+             and high-quality data submissions to the states accordingly to
+             stringent data quality standards laid down by each state`,
           `Extracting claims from database as 837 files and submitting them
-           to the state, ensuring successful file transmission`,
+             to the state, ensuring successful file transmission`,
           `Analyzing and processing the received response files from state
-           and resubmitting in case of rejected claims`,
+             and resubmitting in case of rejected claims`,
         ],
       },
       {
@@ -105,156 +105,22 @@ const KaveriData = {
         subTitle: "Associate Product Support Specialist",
         details: [
           `Through HIX Solutions we helped Exchange (Center of Medicaid
-                  and Medicare) to maintain its data and helped in reconciliation
-                  data between Exchange and Payers (FCH/FLB/NC/IBC/VT).`,
+                    and Medicare) to maintain its data and helped in reconciliation
+                    data between Exchange and Payers (FCH/FLB/NC/IBC/VT).`,
           `Successfully collaborating with the Business End-users, various
-                  stakeholders, technical experts and project management for
-                  efficient Solutions design, consequent development and process
-                  improvements.`,
+                    stakeholders, technical experts and project management for
+                    efficient Solutions design, consequent development and process
+                    improvements.`,
           `Good domain Knowledge of Membership/Enrollment, Benefits,
-                  Claims, Providers, Payers, Medicare and Medicaid`,
+                    Claims, Providers, Payers, Medicare and Medicaid`,
           `Evaluating problems and helping different teams like
-                  Development, Customer Support, Documentation and ESD with
-                  their queries.`,
+                    Development, Customer Support, Documentation and ESD with
+                    their queries.`,
           `Conducted workshops to gather high-level requirements with
-                  Business executives and estimated the product launch planning
-                  and document them as high-level stories in the project backlog`,
+                    Business executives and estimated the product launch planning
+                    and document them as high-level stories in the project backlog`,
         ],
       },
     ],
   },
 };
-const GulrezData = {
-  applicantName: "Gulrez",
-  designation: "Senior Software Engineer",
-  briefSummary: `Full stack web developer, passion & expertise in process workflow automation. Love to
-    Code, Live to Code. Fast Learner. 2 Instant Puraskaar Award for performance
-    improvement. Handling Major Projects of Company with big responsibilities.`,
-  mail: "gulrez0491@gmail.com",
-  mobile: "+1-317-540-5173",
-  website: "gulrezbawa.com",
-  linkedIn: "linkedin.com/in/gulrez-bawa",
-  certifications: {
-    title: "CERTIFICATIONS",
-    list: [
-      {
-        subTitle: "DevOps Certified",
-        year: "2018",
-        details: [],
-      },
-    ],
-  },
-  education: {
-    title: "EDUCATION",
-    list: [
-      {
-        subTitle: "Master of Computer Application (MCA)",
-        year: "2016",
-        details: [],
-      },
-      {
-        subTitle: "Bachelor of Computer Application (BCA)",
-        year: "2012",
-        details: [],
-      },
-    ],
-  },
-  awards: {
-    title: "HONORS & AWARDS",
-    list: [
-      {
-        subTitle: "Instant Puraskaar Award 2 Time by Assistant Vice President",
-        year: "2018 & 2019",
-        details: [
-          `Improved in performance for issue ticketing application by
-          writing stimulation programs for sending/receiving 16K emails
-          to the application`,
-          `Developing Lazy Loading to improve performance`,
-          `Caching module to improve performance`,
-          `Creation of dynamic SQL E-Mail Template which is reused in all
-          applications`,
-          `Maintenance &amp; support for billing tool, handling process of
-          organization’s monthly billing.`,
-          `Training Management, Compliance Tool, Rest API`,
-        ],
-      },
-    ],
-  },
-  expertise: {
-    title: "EXPERTISE",
-    list: [
-      "React JS",
-      "Typescript",
-      "Javascript",
-      "HTML",
-      "CSS",
-      "DHTMLX",
-      "Outlook Add-In",
-      "React Final Form",
-      "BaseWeb Design",
-    ],
-  },
-  skills: {
-    title: "SKILLS",
-    list: [
-      "Critical Thinking",
-      "Problem solving",
-      "Interpersonal",
-      "Requirement Gathering & Analysis",
-      "Planning / Prioritizing",
-      "Decision Making",
-      "Good communication",
-      "Leadership",
-      "Collaboration",
-    ],
-  },
-  tools: {
-    title: "TOOLS",
-    list: [
-      "JIRA",
-      "MS Word",
-      "Excel",
-      "Powerpoint",
-      "Visio",
-      "SQL",
-      "Postman",
-      "Visual Studio Code",
-      "Visual Studio Professional",
-      "Android Studio",
-      "Unity 3D",
-      "Figma",
-    ],
-  },
-  professionServices: {
-    title: "PROFESSIONAL EXPERIENCE",
-    list: [
-      {
-        name: "Jifflenow Technology",
-        year: "2021 - Present",
-        subTitle: "Senior Software Engineer",
-        details: [
-          `Work as front end developer`,
-          `Working on customer facing modules`,
-          `Expertise in Time zone handling using Luxon Library`,
-          `Creation of Scheduler using DHTMLX library`,
-        ],
-      },
-      {
-        name: "Siemens Technology and Services Private Limited (STSPL)",
-        year: "2016 - 2021",
-        subTitle: "Senior Software Engineer",
-        details: [
-          `Full stack web developer responsible for end-to-end web app
-          development.`,
-          `Prototyped new product features to improve productivity`,
-          `Handling organization’s Employee Database and processing as per
-          different tools`,
-          `Boosted user experience (UX/UI) scores over company-wide
-          previous best`,
-        ],
-      },
-    ],
-  },
-};
-
-export const applicantData = GulrezData;
